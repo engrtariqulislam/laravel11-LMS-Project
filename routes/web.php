add9c2aca6487b55e1b70696bf65d27f3f9c2abb
Tariqul islam <tnawayn83@yahoo.com>
2024-06-30 00:21:54 +0300

    5.Laravel Multi Auth with Breeze Part 3
    Laravel Multi Auth with Breeze Part 3

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,22 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-///// Admin
+///// Admin Group Middleware
+Route::middleware(['auth','roles:admin'])->group(function(){
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 
 
 
-///// Instructor
+    }); // End Admin Group Middleware
 
-Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
+
+
+///// Instructor Group Middleware
+Route::middleware(['auth','roles:instructor'])->group(function(){
+
+    Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
+
+    }); // End Instructor Group Middleware 
 
 
